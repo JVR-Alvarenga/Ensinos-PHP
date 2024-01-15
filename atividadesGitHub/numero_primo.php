115803c10011a01092436d758f01b501f2e8c65e
Joao Victor Ribeiro Alvarenga <joaooficial0411@gmail.com>
2024-01-15 01:07:19 -0300

    Create numero_primo.php
    calculando se o numero enviado é primo ou nao

---
+++ atividadesGitHub/numero_primo.php
@@ -0,0 +1,22 @@
+<?php
+
+$n = 4;
+
+function Primo($numero){
+    if($numero <= 1){
+        return 'n eh primo';
+    }
+    if($numero == 2){
+        return 'primo';
+    }
+
+    for($i=2;$i<$numero;$i++){
+        if($numero % $i == 0){
+            return 'n eh primo';
+        }else{
+            return 'primo';
+        }
+    }
+}
+
+echo Primo($n);
