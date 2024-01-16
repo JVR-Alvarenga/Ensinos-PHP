95547934e3fcf64685212c26adc0d30c80fb0401
Joao Victor Ribeiro Alvarenga <joaooficial0411@gmail.com>
2024-01-15 22:05:04 -0300

    upgrade
    atividade de array_reduce, onde pegamos as informacoes de acordo com o array

--- atividadesGitHub/array_reduce.php
+++ atividadesGitHub/array_reduce.php
@@ -1,20 +1,87 @@
 <?php
 
 $pessoas = [
-    ['nome' => 'Joao', 'sexo' => 'M', 'nota' =>  9],
-    ['nome' => 'Julia', 'sexo' => 'F', 'nota' =>  10],
-    ['nome' => 'Victor', 'sexo' => 'M', 'nota' =>  7],
-    ['nome' => 'Juliano', 'sexo' => 'M', 'nota' =>  3],
-    ['nome' => 'Ana', 'sexo' => 'F', 'nota' =>  6],
-    ['nome' => 'Renata', 'sexo' => 'F', 'nota' =>  8]
+    ['nome' => 'Joao', 'sexo' => 'M', 'nota' =>  8],
+    ['nome' => 'Julia', 'sexo' => 'F', 'nota' =>  8],
+    ['nome' => 'Victor', 'sexo' => 'M', 'nota' =>  5],
+    ['nome' => 'Juliano', 'sexo' => 'M', 'nota' =>  10],
+    ['nome' => 'Ana', 'sexo' => 'F', 'nota' =>  10],
+    ['nome' => 'Renata', 'sexo' => 'F', 'nota' =>  7]
 ];
 
 $total_f = array_reduce($pessoas, 'somar_f');
-function somar_f($subtotal, $item){
+function somar_f($subTotal, $item){
     if($item['sexo'] == 'F'){
-        $subtotal ++;
+        $subTotal ++;
     }
-    return $subtotal;
+    return $subTotal;
+}
+echo 'Quantidade de Mulheres: '.$total_f."</br>";
+
+$notaFemininas = array_reduce($pessoas, 'notas_f');
+    function notas_f($subTotal, $item){
+    if($item['sexo'] == 'F'){
+        $subTotal += $item['nota'];
+    }
+    return $subTotal;
 }
 
-echo $total_f;
+echo 'Notas das Mulheres: '.$notaFemininas."</br>";
+
+$total_m = array_reduce($pessoas, 'somar_m');
+    function somar_m($subTotal, $item){
+        if($item['sexo'] == 'M'){
+            $subTotal++;
+        }
+        return $subTotal;
+    }
+
+echo 'Quantidade de Homens: '.$total_m."</br>";
+
+$notaMasculinas = array_reduce($pessoas, 'notas_m');
+    function notas_m($subTotal, $item){
+        if($item['sexo'] == 'M'){
+            $subTotal += $item['nota'];
+        }
+        return $subTotal;
+    }
+
+echo 'Notas dos Homens: '.$notaMasculinas."</br>";
+
+
+if($notaMasculinas > $notaFemininas){
+    echo 'O total das notas dos Homens é maior que a das Mulheres !';
+}elseif($notaMasculinas < $notaFemininas){
+    echo 'O total das notas das Mulheres é maior que a dos Homens !';
+}else{
+    echo 'O total das Notas de ambos sao Iguais !';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- atividadesGitHub/numero_primo.php
+++ atividadesGitHub/numero_primo.php
@@ -11,7 +11,7 @@ function Primo($numero){
     }
 
     for($i=2;$i<$numero;$i++){
-        if($numero % $i == 0){
+        if($numero % $i == 0){ 
             return 'n eh primo';
         }else{
             return 'primo';
