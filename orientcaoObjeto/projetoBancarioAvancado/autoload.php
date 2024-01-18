65fd01406d69fd3f1a5af75a618521e84f615242
Joao Victor Ribeiro Alvarenga <156453131+JVR-Alvarenga@users.noreply.github.com>
2024-01-18 12:00:43 -0300

    Delete orientcaoObjeto/projetoBancarioAvancado directory

--- orientcaoObjeto/projetoBancarioAvancado/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-spl_autoload_register(function($class){     //criando autoload
-    $file = 'classes\index.php';    //definindo a variavel com o local do arquivo
-
-    if(file_exists($file)){     //verificando se o arquivo existe
-        require $file;          //incluindo o arquivo 
-    }
-});
