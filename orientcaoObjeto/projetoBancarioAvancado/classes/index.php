dfb46c3a68c02d41edb07845fa6cffa5a2af4d7e
Joao Victor Ribeiro Alvarenga <joaooficial0411@gmail.com>
2024-01-29 20:24:19 -0300

    php

--- orientcaoObjeto/projetoBancarioAvancado/classes/index.php
+++ orientcaoObjeto/projetoBancarioAvancado/classes/index.php
@@ -7,11 +7,13 @@ class Banco {
     private $address;
     private $corrente;
     private $poupanca;
+    private $transferencia;
     //criando as propriedades
 
-    public function __construct($c, $p){    //injetando as outras classes apartir do construtor
+    public function __construct($c, $p, $t){    //injetando as outras classes apartir do construtor
         $this->corrente = $c;
         $this->poupanca = $p;
+        $this->transferencia = $t;
     }
 
     public function depositoCorrente($d){   //definindo a funcao de deposito da conta corrente
@@ -26,12 +28,23 @@ class Banco {
     public function saquePoupanca($s){      //definindo a funcao de saque da conta poupanca
         $this->poupanca->setSaque($s);
     }
+
+
+    public function transfCorrente($t){
+        $this->transferencia->transfCorrente($t);
+    }
+    public function transfPoupanca($t){
+        $this->transferencia->transfPoupanca($t);
+    }
+
+
     public function saldoCorrente(){        //mostrando saldo da conta corrente
         return $this->corrente->getVerificar();
     }
     public function saldoPoupanca(){        //mostrando saldo da conta poupanca
         return $this->poupanca->getVerificar();
     }
+
     
     public function tscDepCorrente(){   //depositos da conta corrente
         return $this->corrente->tscDeposito();
@@ -47,6 +60,8 @@ class Banco {
         return $this->poupanca->tscSaque();
     }
     
+
+
     public function setCliente($n, $cpf, $ad){  //definindo o cliente 
         $this->cliente = $n;
         $this->cpf = $cpf;
@@ -58,6 +73,7 @@ class Banco {
         'Endereco do Cliente: '.$this->address.'</br>'.'</br>';
     } 
 }
+
 
 namespace conta;    //definindo o namespace da classe Corrente
 class Corrente {
@@ -75,17 +91,20 @@ class Corrente {
         $this->tscSaque += $s;
     }
 
+
     public function tscDeposito(){   //retornando as transacoes de deposito da conta corrente
-        return 'Dinheiro depositado na conta Corrente: '.$this->tscDeposito.'</br>';
+        return '</br>'.'Dinheiro depositado na conta Corrente: '.$this->tscDeposito.'</br>';
     }
     public function tscSaque(){   //retornando as transacoes de saque da conta corrente
         return 'Dinheiro sacado na conta Corrente: '.$this->tscSaque.'</br>';
     }
 
+
     public function getVerificar(){     //retornando o saldo da conta corrente
         return 'Saldo da Conta Corrente: '.$this->contaCorrente.'</br>';
     }
 }
+
 
 namespace conta;    //definindo namespace da classes Poupanca
 class Poupanca {
@@ -94,7 +113,7 @@ class Poupanca {
     private $tscSaque = 0;
 
     public function setDeposito($d){    //depositando na conta poupanca
-        $this->contaPoupanca += $d;
+        $this->contaPoupanca += $d + ($d * 2 / 100);
         $this->tscDeposito += $d;
     }
 
@@ -111,7 +130,31 @@ class Poupanca {
         return 'Dinheiro sacado na conta Poupanca: '.$this->tscSaque.'</br>';
     }
 
+
     public function getVerificar(){        //retornando o saldo da conta poupanca
         return 'Saldo da Conta Poupanca: '.$this->contaPoupanca.'</br>';
     }
 }
+
+
+namespace efetuacoes;
+class Transferencia {
+    private $corrente;
+    private $poupanca;
+
+    public function __construct($c, $p){
+        $this->corrente = $c;
+        $this->poupanca = $p;
+    }
+
+    public function transfCorrente($c){
+        $this->corrente->setDeposito($c);
+        $this->poupanca->setSaque($c);
+    } 
+    public function transfPoupanca($p){
+        $this->poupanca->setDeposito($p);
+        $this->corrente->setSaque($p);
+    }
+
+}
+
--- orientcaoObjeto/projetoBancarioAvancado/index.php
+++ orientcaoObjeto/projetoBancarioAvancado/index.php
@@ -4,18 +4,22 @@ require 'autoload.php';     //incluindo o arquivo
 use cliente\Banco;  //puxando a classe banco
 use conta\Corrente;     //puxando a classe da conta corrente
 use conta\Poupanca;     //puxando a classe da conta poupanca
-use efetuacoes\Transacoes;      //puxando a classe das transacoes
+use efetuacoes\Transferencia;      //puxando a classe das transacoes
 
 $name = 'Joao Victor';      //definindo o cliente
 $cpf = '000.000.000-00';    //o cpf do cliente
 $address = 'Rua Anonima 325';   //o endereco do cliente
 
-$cliente1 = new Banco(new Corrente(), new Poupanca());    //colocando as tres classes dentro da classe principal
+$cliente1 = new Banco(new Corrente(), new Poupanca(), new Transferencia(new Corrente(), new Poupanca()));    
+//colocando as tres classes dentro da classe principal
+
 $cliente1->setCliente($name, $cpf, $address);   //definindo o cliente
 $cliente1->depositoCorrente(1000);    //depositando na conta corrente
 $cliente1->depositoPoupanca(300);    //depositando na conta poupanca
 $cliente1->saqueCorrente(500);     //sacando na conta corrente
 $cliente1->saquePoupanca(500);      //sacando na conta poupanca
+$cliente1->transfCorrente(200);     //transferindo para conta corrente
+
 echo $cliente1->getCliente();       //mostrando os dados do cliente
 echo $cliente1->saldoCorrente();    //mostrando o saldo da conta corrente
 echo $cliente1->saldoPoupanca();    //mostrando o saldo da conta poupanca
