cd3fe0c9f1a0f0b7a8207bc8b7ebc24ac3c5fbcf
Joao Victor Ribeiro Alvarenga <joaooficial0411@gmail.com>
2024-02-01 23:44:26 -0300

    php

---
+++ Banco de Dados/adicionar usuario/adicionar.php
@@ -0,0 +1,14 @@
+<h1>ADICIONAR USUÁRIO</h1>
+
+<form action="adicionar_action.php" method="POST" >
+    <label>
+        Nome: </br>
+        <input type="text" name="name" />
+    </label>  </br></br></br>
+    <label>
+        E-mail: </br>
+        <input type="email" name="email" />
+    </label>  </br></br></br>
+
+    <input type="submit" value="Adicionar Usuário">    
+</form>
